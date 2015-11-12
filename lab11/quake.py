2a2529e823b7e065cab16bb8e62dcc87c78fdc7e
Nick Moore <nickmooregot33@gmail.com>
2015-11-12 12:44:20 -0600

    didn't finish it

--- lab11/quake.py
+++ lab11/quake.py
@@ -1,54 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-Compose a function rolling_mean which accepts as parameters an array with rows of time values and of measurements (just like seis) data, and a window size n.
+seis=np.loadtxt('quakedata.txt')
 
-Create a new array n_mean = np.zeros_like(data). Assign the first row of data to n_mean (the time values are the same).
+assert seis.shape == (2,350)
+from numpy import isclose
+assert isclose(seis[0,1],1.0)
+assert isclose(seis[1,-1],0.61910493526870258)
 
-Now loop through the length of the array (since you want the index, not the row). For the n values close to the beginning of the array, you will assign np.nan (not a number) to the array entry (since there are not yet n values to calculate a moving-window average with). For values n and greater, use the last n data points to calculate the moving-window average and assign that value to the array entry in the second row. This expression may help:
-
-np.mean(data[i-n+1:i+1,1])  # where i is the loop counter
-The function should return the resulting array of time points and calculated rolling-window means.
-"""
 
 def rolling_mean(data, n):
-    pass
+    n_mean = np.zeros_like(data)
+    n_mean[0,:] = data[0,:]
+    tmp = data[1].size
+    for i in range(0,tmp):
+        if i < n:
+            n_mean[1,i] = np.nan
+        else:
+            n_mean[1,i] = np.mean(data[1,i-n+1:i+1])
+
+    return n_mean
 
 
 def test_rolling_mean(array):
-    pass
+    from numpy import isclose
+    rm = rolling_mean(array,2)
+    assert isclose( rm[1,2],0.5*(array[1,1]+array[1,2]))
+    assert isclose(rm[1,-1],0.5*(array[1,-2]+array[1,-1]))
 
 
-"""
-Compose a function rolling_std which accepts as parameters an array with rows of time values and of measurements (just like seis) data, and a window size n.
 
-Create a new array n_std = np.zeros_like(data). Assign the first row of data to n_std (the time values are the same).
-
-Now loop through the array. For values closer than n to the lower end of the array, you will assign np.nan (not a number) to the array entry. For values n and greater, use the last n data points to calculate the moving-window standard deviation and assign that value to the array entry in the second row.
-
-The function should return the resulting array of time points and calculated rolling-window means.
-"""
 
 def rolling_std(data, n):
-    pass
+    n_std = np.zeros_like(data)
+    n_std[0,:] = data[0,:]
+    tmp = data[1].size
+    for i in range(0,tmp):
+        if i < n:
+            n_std[1,i] = np.nan
+        else:
+            n_std[1,i] = np.std(data[1,i-n+1:i+1])
+
+    return n_std
+    
 
 
 def test_rolling_std(array):
-    pass
+    from numpy import isclose, std
+    rs = rolling_std(array,2)
+    assert isclose( rs[1, 2], std( (array[1, 1], array[1, 2])) )
+    assert isclose( rs[1,-1], std( (array[1,-2], array[1,-1])) )
 
 
-"""
-Compose a function quake which accepts as parameters a list of time lengths spans and a list of corresponding standard deviations stds.
-
-The function should first ensure that the lists are the same length and contain only numbers; if either condition is not true, then the function should halt and return None.
-
-Next, create an array data = np.zeros( (np.sum(spans), 2) ). The function should loop over the time length spans in spans and generate a corresponding number of random data points with the standard deviation in stds. The time should increase uniformly (i.e., starting from 0 each time step is one more than the previous time step—the time value and the index will be the same, although one is float and one is int). One way to do this is to assign np.array(range(np.sum(spans)) to the first row of data.
-
-The random data points will be created using numpy.random.normal. Make sure that your scale value in the function is taken from the current element of stds instead.
-
-The function should return the resulting array of time points and generated measurements.
-"""
 
 def quake(spans, stds):
     pass
