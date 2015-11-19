043fabbe9bd24fc9344a359fe02b5260bddd539f
Nick <nickmooregot33@gmail.com>
2015-11-18 21:03:15 -0600

    added empty lab 12 files

---
+++ lab12/spring.py
@@ -0,0 +1,6 @@
+"""
+Compose a function `fit_spring_data` which accepts as a parameter a filename `datafile`.  This function should return the spring constant obtained from fitting the data in that file to a line.
+"""
+
+def fit_spring_data(datafile):
+    pass
